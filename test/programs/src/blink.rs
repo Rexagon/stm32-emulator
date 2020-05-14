ee7426dd450d3863fcc41436f9b12648cfdeddb8
Rexagon <reide740@gmail.com>
2020-05-14 20:00:14 +0300

    Add test stm32 program

---
+++ test/programs/src/blink.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use cortex_m::asm;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m_rt::entry;
+use stm32f1::stm32f103::{interrupt, Interrupt, NVIC};
+
+#[entry]
+fn main() -> ! {
+    asm::nop();
+
+    loop {
+        // your code goes here
+    }
+}
